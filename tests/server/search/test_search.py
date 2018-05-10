dc41a9a9db09e8e7f5e022ff028c3ae3bd392ee7
David Sullivan <dsullivan0190@gmail.com>
2018-05-10 10:01:06 +0100

    Added search tests and mocked ES client

--- tests/server/search/test_search.py
+++ tests/server/search/test_search.py
@@ -12,6 +12,16 @@ class SearchTestSuite(TestApp):
         self.assertIsNotNone(response.body)
         self.assertEqual(response.status, 200)
 
+        self.assertIsNotNone(response.json)
+        self.assertIsInstance(response.json, dict)
+
+        expected_keys = ['result', 'counts', 'featuredResult']
+        for key in expected_keys:
+            self.assertTrue(key in response.json)
+            self.assertIsNotNone(response.json[key])
+            self.assertIsInstance(response.json[key], dict)
+            self.assertGreater(len(response.json[key]), 0)
+
     def test_invalid_search_returns_400(self):
         request, response = self.client.post('/search/ons')
 
