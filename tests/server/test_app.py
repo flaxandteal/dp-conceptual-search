2af35dc626810b96990b0e6a81ac1756e338473a
David Sullivan <dsullivan0190@gmail.com>
2018-05-10 10:27:00 +0100

    Set testing to true in TestApp

--- tests/server/test_app.py
+++ tests/server/test_app.py
@@ -5,5 +5,5 @@ from server.app import create_app
 class TestApp(unittest.TestCase):
     def __init__(self, *args, **kwargs):
         super(TestApp, self).__init__(*args, **kwargs)
-        self.app = create_app()
+        self.app = create_app(testing=True)
         self.client = self.app.test_client
