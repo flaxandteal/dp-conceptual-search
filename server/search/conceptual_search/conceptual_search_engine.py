c42d29c934fd89cb2906c6565cec7c6d6b0ac6a5
David Sullivan <dsullivan0190@gmail.com>
2018-05-18 13:05:13 +0100

    Started work on conceptual search engine

--- server/search/conceptual_search/conceptual_search_engine.py
+++ server/search/conceptual_search/conceptual_search_engine.py
@@ -4,3 +4,22 @@ from server.search.search_engine import SearchEngine
 class ConceptualSearchEngine(SearchEngine):
     from server.word_embedding.supervised_models import SupervisedModels, load_model
     word_embedding_model = load_model(SupervisedModels.ONS)
+
+    def __init__(self, *args, **kwargs):
+        super(ConceptualSearchEngine, self).__init__(*args, **kwargs)
+
+    def content_query(
+            self,
+            search_term: str,
+            current_page: int = 1,
+            size: int = 10,
+            **kwargs):
+        """
+        This is the only function we have to overwrite from SearchEngine!
+        :param search_term:
+        :param current_page:
+        :param size:
+        :param kwargs:
+        :return:
+        """
+        raise NotImplementedError("Conceptual search not yet implemented")
