466ff328e2987b92e318fd0e614974cfd5b08111
David Sullivan <dsullivan0190@gmail.com>
2018-05-21 07:36:33 +0100

    Update unit.sh to build fastText

--- ci/scripts/unit.sh
+++ ci/scripts/unit.sh
@@ -1,6 +1,8 @@
 #!/bin/bash -eux
 
 pushd dp-conceptual-search
+  pip install Cython pybind11
+  python lib/fastText/setup.py install
   pip install -r requirements.txt
   pip install -r requirements_test.txt
   python manager.py test
