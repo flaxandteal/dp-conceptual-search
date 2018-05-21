c3f8a104bdd2cd397f4aefd829a813ffade77e24
David Sullivan <dsullivan0190@gmail.com>
2018-05-21 07:32:24 +0100

    Update build script to clone git submodules

--- ci/scripts/build.sh
+++ ci/scripts/build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash -eux
 
 pushd dp-conceptual-search
+  git submodule sync --recursive
+  git submodule update --init --recursive
   cp -r server lib manager.py requirements.txt config_*.py Dockerfile.concourse ../build/
 popd
