87d53f5102d6b561a597eaa74214497e12cc9efb
David Sullivan <dsullivan0190@gmail.com>
2018-06-04 07:19:07 +0100

    Removed requirements_model.txt from build script

--- ci/scripts/build.sh
+++ ci/scripts/build.sh
@@ -2,5 +2,5 @@
 
 pushd dp-conceptual-search
   make build
-  cp -r server lib supervised_models manager.py build_model.py requirements.txt requirements_model.txt requirements_test.txt config_*.py Makefile Dockerfile.concourse ../build/
+  cp -r server lib supervised_models manager.py build_model.py requirements*.txt config_*.py Makefile Dockerfile.concourse ../build/
 popd
