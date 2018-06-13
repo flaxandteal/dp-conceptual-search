2dd52aeaf24cd08ed4f1014522632ae22c4dc9dc
David Sullivan <dsullivan0190@gmail.com>
2018-06-13 09:45:43 +0100

    Prevent build script from executing tests

--- ci/scripts/build.sh
+++ ci/scripts/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -eux
 
 pushd dp-conceptual-search
-  make && make clean
+  make build clean
   cp -r server lib manager.py supervised_models requirements*.txt config_*.py Makefile Dockerfile.concourse ../build/
 popd
