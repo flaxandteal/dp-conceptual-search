92237ebad62597ca0e7b343179a9f60e3633e731
David Sullivan <dsullivan0190@gmail.com>
2018-06-18 11:46:52 +0100

    Updated Dockerfiles

--- ci/scripts/build.sh
+++ ci/scripts/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -eux
 
 pushd dp-conceptual-search
-  make build
+  make build clean
   cp -r server lib manager.py requirements.txt config_*.py Makefile Dockerfile.concourse ../build/
 popd
