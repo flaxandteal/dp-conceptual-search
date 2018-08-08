0d95a3a373915b1633a1cb629a96f8ff9ce1837e
David Sullivan <dsullivan0190@gmail.com>
2018-08-08 10:37:55 +0100

    Add word2vec directory to build

--- ci/scripts/build.sh
+++ ci/scripts/build.sh
@@ -2,6 +2,6 @@
 
 pushd dp-conceptual-search
   make build clean
-#  cp -r server core ons lib manager.py supervised_models requirements*.txt config_*.py Makefile Dockerfile.concourse ../build/
-cp -r server core ons lib manager.py requirements*.txt config_*.py Makefile Dockerfile.concourse ../build/
+#  cp -r server core ons lib manager.py supervised_models word2vec requirements*.txt config_*.py Makefile Dockerfile.concourse ../build/
+    cp -r server core ons lib manager.py word2vec requirements*.txt config_*.py Makefile Dockerfile.concourse ../build/
 popd
