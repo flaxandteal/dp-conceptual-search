8566494e28b96526f648c7412c8402cc0b3b40f4
David Sullivan <dsullivan0190@gmail.com>
2018-08-08 09:40:23 +0100

    Remove supervised_models directory from build script

--- ci/scripts/build.sh
+++ ci/scripts/build.sh
@@ -2,5 +2,6 @@
 
 pushd dp-conceptual-search
   make build clean
-  cp -r server core ons lib manager.py supervised_models requirements*.txt config_*.py Makefile Dockerfile.concourse ../build/
+#  cp -r server core ons lib manager.py supervised_models requirements*.txt config_*.py Makefile Dockerfile.concourse ../build/
+cp -r server core ons lib manager.py requirements*.txt config_*.py Makefile Dockerfile.concourse ../build/
 popd
