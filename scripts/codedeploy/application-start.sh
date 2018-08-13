a5e80e7f67c2fd6a03237ac9520499a3a47f2326
David Sullivan <dsullivan0190@gmail.com>
2018-08-13 12:14:19 +0100

    Update application start script

--- scripts/codedeploy/application-start.sh
+++ scripts/codedeploy/application-start.sh
@@ -37,6 +37,7 @@ docker run -d                                                           \
   --env=ELASTIC_SEARCH_ASYNC_ENABLED=$ELASTIC_SEARCH_ASYNC_ENABLED      \
   --env=ELASTIC_SEARCH_TIMEOUT=$ELASTIC_SEARCH_TIMEOUT                  \
   --env=SEARCH_INDEX=$SEARCH_INDEX                                      \
+  --env=CONCEPTUAL_SEARCH_ENABLED=$CONCEPTUAL_SEARCH_ENABLED            \
   --env=USER_RECOMMENDATION_ENABLED=$USER_RECOMMENDATION_ENABLED        \
   --env=BIND_HOST=$BIND_HOST                                            \
   --env=BIND_PORT=$BIND_PORT                                            \
