ad1aab1e2e523067a28c216b4e34bc79000045a3
David Sullivan <dsullivan0190@gmail.com>
2018-09-06 13:30:29 +0100

    Add stability around getting substr index

--- server/anonymize/__init__.py
+++ server/anonymize/__init__.py
@@ -7,13 +7,20 @@ def get_salt() -> str:
     return os.environ.get("GA_SALT", "")
 
 
-def get_substr_index() -> int:
-    return int(os.environ.get("GA_SUBSTR_INDEX", 0))
+def get_substr_index(default: int=0) -> int:
+    try:
+        substr_idx = int(os.environ.get("GA_SUBSTR_INDEX", default))
+
+        if not isinstance(substr_idx, int):
+            return default
+        return substr_idx
+    except Exception:
+        return default
 
 
-def hash_value(
-        value: str,
-        salt: str=get_salt(),
-        substr_index: int=get_substr_index()) -> str:
+def hash_value(value: str) -> str:
+    salt = get_salt()
+    substr_index = get_substr_index()
+
     s = "%s%s%s" % (value[substr_index:], salt, value[:substr_index])
     return str(hashlib.sha512(s.encode(sys.getdefaultencoding())).hexdigest())
