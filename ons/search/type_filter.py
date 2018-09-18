bcc56098a890f2dd65e6a09e1ca43222cc6d4cea
David Sullivan <dsullivan0190@gmail.com>
2018-09-18 06:46:25 +0100

    Cleaned up content_types.py

--- ons/search/type_filter.py
+++ ons/search/type_filter.py
@@ -1,30 +1,32 @@
 from typing import List
 from enum import Enum
-from ons.search.content_type import *
+from ons.search.content_type import ContentTypes, ContentType
 
 
 class TypeFilter(object):
     """
     Specifies a list of content types for this type filter
     """
-    def __init__(self, content_type_filters: List[ContentType]):
+    def __init__(self, content_type_filters: List[ContentTypes]):
         self.content_type_filters = content_type_filters
 
-    def get_content_types(self) -> List[ContentType]:
-        return self.content_type_filters
+    def get_content_types(self) -> List[ContentTypes]:
+        return [content_type.value for content_type in self.content_type_filters]
 
 
 class TypeFilters(Enum):
     """
     Enum of available type filters for the ONS website
     """
-    BULLETIN = TypeFilter([bulletin])
-    ARTICLE = TypeFilter([article, article_download])
-    COMPENDIA = TypeFilter([compendium_landing_page])
-    TIME_SERIES = TypeFilter([timeseries])
-    DATASETS = TypeFilter([dataset_landing_page, reference_tables])
-    USER_REQUESTED_DATA = TypeFilter([static_adhoc])
-    QMI = TypeFilter([static_qmi])
-    METHODOLOGY = TypeFilter([static_qmi, static_methodology, static_methodology_download])
-    METHODOLOGY_ARTICLE = TypeFilter([static_methodology, static_methodology_download])
-    CORPORATE_INFO = TypeFilter([static_foi, static_page, static_landing_page, static_article])
+    BULLETIN = TypeFilter([ContentTypes.BULLETIN])
+    ARTICLE = TypeFilter([ContentTypes.ARTICLE, ContentTypes.ARTICLE_DOWNLOAD])
+    COMPENDIA = TypeFilter([ContentTypes.COMPENDIUM_LANDING_PAGE])
+    TIME_SERIES = TypeFilter([ContentTypes.TIMESERIES])
+    DATASETS = TypeFilter([ContentTypes.DATASET_LANDING_PAGE, ContentTypes.REFERENCE_TABLES])
+    USER_REQUESTED_DATA = TypeFilter([ContentTypes.STATIC_ADHOC])
+    QMI = TypeFilter([ContentTypes.STATIC_QMI])
+    METHODOLOGY = TypeFilter([ContentTypes.STATIC_QMI, ContentTypes.STATIC_METHODOLOGY,
+                              ContentTypes.STATIC_METHODOLOGY_DOWNLOAD])
+    METHODOLOGY_ARTICLE = TypeFilter([ContentTypes.STATIC_METHODOLOGY, ContentTypes.STATIC_METHODOLOGY_DOWNLOAD])
+    CORPORATE_INFO = TypeFilter([ContentTypes.STATIC_FOI, ContentTypes.STATIC_PAGE, ContentTypes.STATIC_LANDING_PAGE,
+                                 ContentTypes.STATIC_ARTICLE])
