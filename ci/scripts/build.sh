35be8bfa553f2a525632833e79de398fe4a1263a
David Sullivan <dsullivan0190@gmail.com>
2018-09-21 11:15:47 +0100

    Update build script

--- ci/scripts/build.sh
+++ ci/scripts/build.sh
@@ -2,6 +2,5 @@
 
 pushd dp-conceptual-search
   make build clean
-#  cp -r api search ons lib manager.py supervised_models word2vec requirements*.txt config_*.py Makefile Dockerfile.concourse ../build/
-    cp -r server core ons lib manager.py download_nltk_stopwords.py word2vec requirements*.txt config_*.py Makefile Dockerfile.concourse ../build/
+    cp -r api app config search ons lib manager.py download_nltk_stopwords.py word2vec requirements*.txt config_*.py Makefile Dockerfile.concourse ../build/
 popd
