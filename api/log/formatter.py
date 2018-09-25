97275b0de06a0ad69aac7956fbc76a05b4d8a24f
David Sullivan <dsullivan0190@gmail.com>
2018-09-25 10:14:49 +0100

    Fix issue with coloured logging

--- api/log/formatter.py
+++ api/log/formatter.py
@@ -1,14 +1,14 @@
 """
 Add namespace to all log messages
 """
-from config.config_core import API_TITLE, JSON_LOGGER_INDENT
+from config.config_core import API_TITLE, COLOURED_LOGGING_ENABLED, JSON_LOGGER_INDENT
 from dp.log.formatters import CustomJsonFormatter
 
 
 class SearchJsonFormatter(CustomJsonFormatter):
     def __init__(self, *args, **kwargs):
         super(SearchJsonFormatter, self).__init__(
-            *args, json_indent=JSON_LOGGER_INDENT, **kwargs)
+            *args, json_indent=JSON_LOGGER_INDENT, coloured_logging=COLOURED_LOGGING_ENABLED, **kwargs)
 
     def add_fields(self, log_record, record, message_dict):
         super(SearchJsonFormatter, self).add_fields(log_record, record, message_dict)
