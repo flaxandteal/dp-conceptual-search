0381bd6d875b40cfb3fe859cf5cc2500fa5421a0
David Sullivan <dsullivan0190@gmail.com>
2018-09-27 19:17:00 +0100

    Handle None requests

--- app/error_handlers.py
+++ app/error_handlers.py
@@ -3,6 +3,7 @@ Defines custom Sanic error handlers
 """
 import sanic.exceptions
 from sanic.response import json
+from sanic.log import logger as sanic_logger
 
 from app.sanic_search import SanicSearch
 from api.log import logger
@@ -16,5 +17,8 @@ class ErrorHandlers(object):
         # Explicitly handle RequestTimeouts -> These are logged out as Errors by default (unwanted)
         @app.exception(sanic.exceptions.RequestTimeout)
         def timeout(request: ONSRequest, exception: sanic.exceptions.SanicException):
-            logger.debug(request, "RequestTimeout from error_handler.", exc_info=exception)
+            if request is None:
+                sanic_logger.debug("RequestTimeout from error_handler for null request.", exc_info=exception)
+            else:
+                logger.debug(request, "RequestTimeout from error_handler.", exc_info=exception)
             return json({"message": "RequestTimeout from error_handler."}, exception.status_code)
