805a76ae93536b439d3a37f961f141f2ba32f431
David Sullivan <dsullivan0190@gmail.com>
2018-09-28 11:22:30 +0100

    Added comments

--- app/exceptions/error_handlers.py
+++ app/exceptions/error_handlers.py
@@ -18,7 +18,10 @@ class ErrorHandlers(object):
         @app.exception(sanic.exceptions.RequestTimeout)
         def timeout(request: ONSRequest, exception: sanic.exceptions.SanicException):
             if request is None:
+                # Request has timed out and as such was awaited by the server (no longer exists)
                 sanic_logger.debug("RequestTimeout from error_handler for null request.", exc_info=exception)
             else:
+                # Cover any future API changes which could see the request preserved during timeout (also log out
+                # request context)
                 logger.debug(request, "RequestTimeout from error_handler.", exc_info=exception)
             return json({"message": "RequestTimeout from error_handler."}, exception.status_code)
