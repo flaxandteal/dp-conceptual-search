f1644845c45bec19db6ae150e4f4a28a11944456
David Sullivan <dsullivan0190@gmail.com>
2018-10-15 10:48:27 +0100

    Remove lib from copy

--- ci/scripts/build.sh
+++ ci/scripts/build.sh
@@ -2,6 +2,6 @@
 
 pushd dp-conceptual-search
   make build
-    cp -r api app config search ons ml lib manager.py requirements*.txt Makefile Dockerfile.concourse ../build/
+    cp -r api app config search ons ml manager.py requirements*.txt Makefile Dockerfile.concourse ../build/
     mkdir ../build/scripts && cp scripts/download_nltk_stopwords.py ../build/scripts/
 popd
