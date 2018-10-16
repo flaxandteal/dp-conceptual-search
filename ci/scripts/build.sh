8c21aa9dcf64ce81b4b9e1cb67ede5032a5d78ee
David Sullivan <dsullivan0190@gmail.com>
2018-10-16 13:48:41 +0100

    Remove make build from build script

--- ci/scripts/build.sh
+++ ci/scripts/build.sh
@@ -1,7 +1,6 @@
 #!/bin/bash -eux
 
 pushd dp-conceptual-search
-  make build
     cp -r api app config search ons ml dp manager.py requirements*.txt Makefile Dockerfile.concourse ../build/
     mkdir ../build/scripts && cp scripts/download_nltk_stopwords.py ../build/scripts/
 popd
