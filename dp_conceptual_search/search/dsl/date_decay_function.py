2a1a2dd506281e6a7d8810282a06f25e758edab4
David Sullivan <dsullivan0190@gmail.com>
2018-11-21 08:33:47 +0000

    Added docstring

--- dp_conceptual_search/search/dsl/date_decay_function.py
+++ dp_conceptual_search/search/dsl/date_decay_function.py
@@ -5,6 +5,16 @@ from elasticsearch_dsl import query as Q
 
 
 def date_decay_function(field: str, fn: str, scale: str, offset: str, decay: float, origin: str="now"):
+    """
+    Builds a decay function useful for boosting more recent content
+    :param field:
+    :param fn:
+    :param scale:
+    :param offset:
+    :param decay:
+    :param origin:
+    :return:
+    """
     return Q.SF(
         fn, **{
             field: {
